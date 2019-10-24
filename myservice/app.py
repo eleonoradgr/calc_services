f145e0d9c8c149b333b5bc970ac03cb9274ca067
Eleonora <eleonora.digregorio8@gmail.com>
2019-10-24 08:53:18 +0200

    without profiler

--- myservice/app.py
+++ myservice/app.py
@@ -1,7 +1,6 @@
 import os
 from flakon import create_app
 #from flask_profiler import Profiler
-import flask_profiler
 from myservice.views import blueprints
 
 _HERE = os.path.dirname(__file__)
@@ -9,22 +8,22 @@ _SETTINGS = os.path.join(_HERE, 'settings.ini')
 
 app = create_app(blueprints=blueprints, settings=_SETTINGS)
 
-app.config["flask_profiler"] = {
-    "enabled": app.config["DEBUG"],
-    "storage": {
-        "engine": "sqlite"
-    },
-    "basicAuth": {
-        "enabled": True,
-        "username": "admin",
-        "password": "admin"
-    },
-    "ignore": [
-        "^/static/.*"
-    ]
-}
+#app.config["flask_profiler"] = {
+#    "enabled": app.config["DEBUG"],
+#    "storage": {
+#        "engine": "sqlite"
+#    },
+#    "basicAuth": {
+#        "enabled": True,
+#        "username": "admin",
+#        "password": "admin"
+#    },
+#    "ignore": [
+#        "^/static/.*"
+#    ]
+#}
 
-profiler = Profiler(app)
-
-if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000)
+#profiler = Profiler(app)
+#
+#if __name__ == '__main__':
+#    app.run(host="127.0.0.1", port=5000)
